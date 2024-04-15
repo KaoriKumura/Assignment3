daad1f207b19721e3a31e0122e35a5e2a4dfc578
Kaori Kumura <kaori.k239@gmail.com>
2024-04-15 13:44:26 -0400

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
